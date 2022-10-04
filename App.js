9819c625b832e2e3fd0d62979648d19369b6900f
WalkerAlone764 <109795525+WalkerAlone764@users.noreply.github.com>
2022-10-04 09:54:56 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View, StyleSheet} from 'react-native';
+import main from './app/main';
+import TabNavigation from './app/navigator/TabNavigation';
+
+function App() {
+  return <TabNavigation />;
+}
+
+const styles = StyleSheet.create({
+  container: {},
+});
+
+export default App;
